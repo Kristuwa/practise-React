7169837030f9ac93d60c7fb5712c3283175fd21f
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2022-12-10 16:33:27 +0200

    create searchForm and API service

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,39 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+  handleChange = e => {
+    const query = e.target.value;
+    this.setState({ query });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const query = this.state.query.trim();
+    if (query === '') {
+      return;
+    }
+    this.props.getQuery(query);
+    this.setState({ query: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          value={this.state.query}
+          onChange={this.handleChange}
+        />
+      </SearchFormStyled>
+    );
   }
 }
